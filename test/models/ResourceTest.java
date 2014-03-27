a89a479f1fa4cc181425e85f8babebf42458eb41
Jackie Aldama <jaldama@bu.edu>
2014-03-27 12:17:09 -0400

    Added Test Classes for model objects Resource and User
    Will work on implementing the methods

---
+++ test/models/ResourceTest.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.junit.*;
+
+import play.mvc.*;
+import play.test.*;
+import play.data.DynamicForm;
+import play.data.validation.ValidationError;
+import play.data.validation.Constraints.RequiredValidator;
+import play.i18n.Lang;
+import play.libs.F;
+import play.libs.F.*;
+
+import static play.test.Helpers.*;
+import static org.fest.assertions.Assertions.*;
+
+
+/**
+*
+* Test Class for Resource Entity
+*
+*/
+public class ResourceTest {
+
+    @Test
+    public void testResourceEntity() {
+        
+    }
+
+    @Test
+    public void testResourceCreate() {
+        
+    }
+	
+	@Test
+    public void testResourceDelete() {
+        
+    }
+
+
+}
