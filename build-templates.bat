56860519a58f5fb82c28ea7bcbe9e0a7dc97bfd0
halek1 <halek1@bu.edu>
2014-04-05 14:02:20 -0400

    added build-templates

---
+++ build-templates.bat
@@ -0,0 +1,2 @@
+handlebars --min --amd --extension hbs public\templates\main --output public\javascripts\compiled-templates.js
+handlebars --min --partial --amd --extension hbs public\templates\partials --output public\javascripts\compiled-partials.js
