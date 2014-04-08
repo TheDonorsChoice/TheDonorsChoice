bbf813e9990bc107410b3e41e1b6559fb6748556
risheebasdeo <rbasdeo@bu.edu>
2014-04-08 19:27:38 -0400

    Changes made to filter the resource list
    Keyword search

--- app/assets/javascripts/collections/ResourceCollection.js
+++ app/assets/javascripts/collections/ResourceCollection.js
@@ -5,7 +5,30 @@ define([
 ], function(_, Backbone, ResourceModel){
     var ResourceCollection = Backbone.Collection.extend({
         model: ResourceModel,
-        url: "/resources.json"
+        url: "/resources.json",
+        
+        find: function(filtertext) {
+        	if (filtertext == "")
+        		return this;
+        	// split the search words and store into an array
+        	var filterarray = filtertext.split(" ");
+        	
+        	// filter logic
+        	return _(this.filter(function(m) {
+        		
+        		// check every search word against the search fields
+        		for (var i=0; i<filterarray.length; i++) {
+        			var temp = filterarray[i]; // this variable contains a keyword
+        			if (!temp == " "){
+        			
+                	var pattern = new RegExp(temp,"i");
+                	// is the keyword found in any of our fields?
+        			if (pattern.test(m.get("address")) || pattern.test(m.get("orgName")) || pattern.test(m.get("itemsNeeded")))
+        				return true; // the keyword was found, this row should be displayed
+        			}
+        		}
+        	}));
+        }
     });
     // Return the Collection class to allow others to instantiate it.
     return ResourceCollection;
--- app/assets/javascripts/views/ResourceView.js
+++ app/assets/javascripts/views/ResourceView.js
@@ -5,17 +5,22 @@ define([ // <- requireJS stuff
     'jquery',
     'underscorejs',
     'backbonejs',
-    'compiled-templates'
-], function($, _, Backbone, Templates){
+    'compiled-templates',
+    'compiled-partials'
+], function($, _, Backbone, Templates, Partials){
 
     var view = Backbone.View.extend({
     	// this is the template being used, which can be found here: /public/templates/resource-template.hbs
         template: Templates['resource-template'],
+        listtemplate: Partials['resource-list-template'],
         // our code will be appended to /app/views/main.scala.html
         // somewhere in that file there's an html element where id="main"
         // "el" is now a reference to that element
-        el: $('#main'),
+        el: $("#main"),
 
+        events: {
+        	"keyup #filterinput" : "filterlist"
+        },
         // this function is implicitly called
         initialize: function() {
             _.bindAll(this, 'render');
@@ -26,18 +31,36 @@ define([ // <- requireJS stuff
                 collection.trigger("change");
             }});
         },
-
         render: function() {    
 	    	var geocoder = new google.maps.Geocoder();
 			var mapOptions = {
 				center: new google.maps.LatLng(42.3736158, -71.1097335),
 				zoom: 10
 			};  
-        	// build the page using the template and store the results in 'resourcehtml'
+        	// build the page using a template and store the results in "resourcehtml"
             var resourcehtml = this.template(this.collection);
             this.$el.html(resourcehtml); // <-- jquery stuff to append "resourcehtml" to "#main"
-            
 			var map = new google.maps.Map($("#map-canvas")[0], mapOptions);
+        },
+        
+        // filter 
+        filterlist: function() {
+        	var filtertext = $("#filterinput").val();
+        	var temp = [];
+        	var col =  this.collection.find(filtertext);
+        	// each item in the collection is an object of attributes
+        	// we push the collection of attributes into an array so we can pass it to a template
+        	col.each(function(item) {
+            	temp.push(item);
+        	});
+        	// pass the attributes array to the template as "models"
+        	var wrapper = {models:temp}; 
+        	
+        	// build the table using a partial template and store the results in listhtml
+        	var listhtml = this.listtemplate(wrapper);
+        	
+        	// render the table (this table will be a filtered list)
+        	$("#resourcelist").html(listhtml);
         }
     });
     return view;
