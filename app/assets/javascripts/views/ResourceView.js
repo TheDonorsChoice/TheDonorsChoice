2a6b8bc17c23a09e8f6ead23c4e7fd3383b17dc6
risheebasdeo <rbasdeo@bu.edu>
2014-04-11 00:40:20 -0400

    Map markers enabled
    markers added to map based on list

--- app/assets/javascripts/views/ResourceView.js
+++ app/assets/javascripts/views/ResourceView.js
@@ -9,6 +9,8 @@ define([ // <- requireJS stuff
     'compiled-partials'
 ], function($, _, Backbone, Templates, Partials){
 
+	var markers = [];
+	var map;
     var view = Backbone.View.extend({
     	// this is the template being used, which can be found here: /public/templates/resource-template.hbs
         template: Templates['resource-template'],
@@ -19,11 +21,12 @@ define([ // <- requireJS stuff
         el: $("#main"),
 
         events: {
-        	"keyup #filterinput" : "filterlist"
+        	"keyup #filterinput" : "filterlist",
+        	"blur #filterinput" : "markmap"
         },
         // this function is implicitly called
         initialize: function() {
-            _.bindAll(this, 'render');
+            _.bindAll(this, 'render','filterlist');
 
             this.listenTo(this.collection, "change", this.render);
 
@@ -32,19 +35,24 @@ define([ // <- requireJS stuff
             }});
         },
         render: function() {    
-	    	var geocoder = new google.maps.Geocoder();
 			var mapOptions = {
 				center: new google.maps.LatLng(42.3736158, -71.1097335),
-				zoom: 10
+				zoom: 12
 			};  
         	// build the page using a template and store the results in "resourcehtml"
             var resourcehtml = this.template(this.collection);
             this.$el.html(resourcehtml); // <-- jquery stuff to append "resourcehtml" to "#main"
-			var map = new google.maps.Map($("#map-canvas")[0], mapOptions);
+            
+            // bind the map to map-canvas
+			map = new google.maps.Map($("#map-canvas")[0], mapOptions);
+			
+			// add the markers to the map
+			this.markmap();
         },
         
         // filter 
-        filterlist: function() {
+        filterlist: function() { 
+        	// get the values entered in the keyword search
         	var filtertext = $("#filterinput").val();
         	var temp = [];
         	var col =  this.collection.find(filtertext);
@@ -61,6 +69,51 @@ define([ // <- requireJS stuff
         	
         	// render the table (this table will be a filtered list)
         	$("#resourcelist").html(listhtml);
+        },
+        
+        // mark the map with the addresses in the collection
+        markmap: function() {	
+	    	var geocoder = new google.maps.Geocoder();	
+	    	
+        	// remove any markers that are currently on the map
+        	for (var i=0; i<markers.length; i++) {
+        		markers[i].setMap(null);
+        	}
+        	
+        	// mark the map based on the filter
+        	var filtertext = $("#filterinput").val();
+        	
+			// for each item mark its location on the map
+        	var col =  this.collection.find(filtertext);
+			col.each(function(item) {
+				// get the address from this item
+				var address = item.get("address");
+				
+				// get the geocode
+				// geocodes are needed to mark locations on a google map
+				geocoder.geocode( { 'address': address}, function(results, status) {
+					if (status == google.maps.GeocoderStatus.OK) {
+						
+						// add the marker to the map
+						var marker = new google.maps.Marker({
+							map: map,
+							position: results[0].geometry.location
+						});
+						
+						// add a listener for the marker, so when it's clicked we display something
+						google.maps.event.addListener(marker, 'click', function() {
+							alert(address); // ideally, we'll include details about the posting here
+						});
+						
+						// add the marker to an array
+						// we're doing this so we have the ability to remove it, when needed
+						markers.push(marker);
+					} 
+					else {
+						alert("Address not found.  Geocode error: " + status);
+					}
+				});
+			});
         }
     });
     return view;
