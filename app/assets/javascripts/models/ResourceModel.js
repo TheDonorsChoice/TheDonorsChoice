5c5df977a76335b0cac13cc59f6676c744aa9f6b
Jackie Aldama <jaldama@bu.edu>
2014-04-13 13:29:37 -0400

    Reverting Resource changes for now

--- app/assets/javascripts/models/ResourceModel.js
+++ app/assets/javascripts/models/ResourceModel.js
@@ -11,17 +11,7 @@ define([
             Type: "",
             itemsNeeded: ""
         }
-    	update_resource: function(data) {
-            this.set("orgName", data.orgName);
-            this.set("address", data.address);
-            this.set("phoneNumber", data.phoneNumber);
-            this.set("email", data.email);
-            this.set("Type", data.Type);
-            this.set("itemsNeeded", data.itemsNeeded)
-        }
 
-        this.trigger('change', this);
-    }
     });
 
     // Return the model for the module
