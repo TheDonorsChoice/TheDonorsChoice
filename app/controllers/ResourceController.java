2813a6b73f62a0a0c7c5210fa7b977bee377ae45
Christopher Theberge <chris@codeforyou.com>
2014-04-27 10:59:50 -0400

    Reformatted resource controller for readability.

--- app/controllers/ResourceController.java
+++ app/controllers/ResourceController.java
@@ -1,55 +1,57 @@
 package controllers;
 
 
-
 import models.Resource;
 import models.User;
-import play.*;
+import play.Logger;
 import play.data.DynamicForm;
 import play.data.Form;
-import play.mvc.*;
-import views.html.*;
+import play.mvc.Controller;
+import play.mvc.Result;
+import play.mvc.Results;
+import views.html.resource;
 
 public class ResourceController extends Controller {
-	static Form<Resource> resourceForm = Form.form(Resource.class);
-    
-	public static Result resources() {
-		return ok(resource.render(Resource.all(), resourceForm));
-	}
+    static Form<Resource> resourceForm = Form.form(Resource.class);
+
+    public static Result resources() {
+        return ok(resource.render(Resource.all(), resourceForm));
+    }
 
     public static Result jsonResources() {
         return ok(play.libs.Json.toJson(Resource.all()));
     }
-	//delete resources
+
+    //delete resources
     public static Result deleteResource(Long id) {
-  	  Resource.delete(id); 
-  	  return redirect(routes.ResourceController.resources());
+        Resource.delete(id);
+        return redirect(routes.ResourceController.resources());
     }
+
     //create resources
     public static Result createResource() {
-    	 
-    	 String guide = session().get("guid");
-         User userForGuid = User.findByString.where().eq("guid", guide).findUnique();
-         Logger.info(userForGuid.type.toString() + " " + userForGuid.name);
-         if((userForGuid.type == User.UserType.PANTRY) || (userForGuid.type == User.UserType.SHELTER))
-         {
-    	 DynamicForm requestData = Form.form().bindFromRequest();
-    	 String title = requestData.get("title");
-    	 String type = requestData.get("type");
-    	 String description = requestData.get("description");
-    	 String itemsNeeded = requestData.get("itemsNeeded");
-    	 String items = "";
-    	 Resource resource = new Resource();
-    	 resource.user = userForGuid;
-    	 resource.description = description;
-    	 resource.Type = userForGuid.type.toString();
-    	 resource.itemsNeeded = itemsNeeded;
-    	 resource.title = title;
-    	 Resource.create(resource); 
-    	 return Results.created();
-         }else{
-        	 return Results.badRequest("Invalid User Type");
-         }
+
+        String guide = session().get("guid");
+        User userForGuid = User.findByString.where().eq("guid", guide).findUnique();
+        Logger.info(userForGuid.type.toString() + " " + userForGuid.name);
+        if ((userForGuid.type == User.UserType.PANTRY) || (userForGuid.type == User.UserType.SHELTER)) {
+            DynamicForm requestData = Form.form().bindFromRequest();
+            String title = requestData.get("title");
+            String type = requestData.get("type");
+            String description = requestData.get("description");
+            String itemsNeeded = requestData.get("itemsNeeded");
+            String items = "";
+            Resource resource = new Resource();
+            resource.user = userForGuid;
+            resource.description = description;
+            resource.Type = userForGuid.type.toString();
+            resource.itemsNeeded = itemsNeeded;
+            resource.title = title;
+            Resource.create(resource);
+            return Results.created();
+        } else {
+            return Results.badRequest("Invalid User Type");
         }
-      
+    }
+
 }
